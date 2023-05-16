ffbdf2327be7b01904c888ae42129142467d177a
Feanor01 <akshanshghildiyal6248rcb@gmail.com>
2023-05-16 15:09:21 +0530

    Updated The files

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-alert("All good");
+let its = document.getElementById("todo");
+its.style.backgroundColor = "yellow";
+butn = document.getElementsByClassName("favorite");
+let bottom = document.getElementsByClassName("bottom");
+butn[0].onclick = ()=>{
+console.log(its.value);
+        const textnode = document.createTextNode(its.value);   const newdiv = document.createElement("div");
+    newdiv.appendChild(textnode);
+    bottom[0].appendChild(newdiv);
+its.value=null;
+}
+
+
+
+
+
+
+
+
+
+//e.preventdefault use this function 
+//onsubmit
